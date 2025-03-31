d8144213c0cf7cdd24c32deb61a11a2f1eec6dd5
bobwen0205 <wenmingzhi10@gmail.com>
2025-03-31 18:46:04 -0400

    first commit

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import userRouter from './routes/userRoutes.js';
+import imageRouter from './routes/imageRoutes.js';
+
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+app.use(cors());
+await connectDB();
+
+app.get('/', (req, res) => res.send('API working'));
+app.use('/api/user', express.json(), userRouter);
+app.use('/api/image', express.json(), imageRouter);
+
+
+app.listen(PORT, () => console.log('Server running on port ' + PORT));
